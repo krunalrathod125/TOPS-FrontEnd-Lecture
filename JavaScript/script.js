7eaca263f0974ecc2bef2822fe71ec521c4008e4
Phishing Detection Website <cse.220840131125@gmail.com>
2026-04-08 19:25:06 +0530

    JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,22 @@
+// JavaScript Variables
+var name = "Krunal";
+var age = 21;
+ console.log(name);
+ console.log(age);
+
+name = "Krunal ";
+
+let city = "Surat";
+console.log(city);
+
+city = "Ahmedabad";
+console.log(city);
+
+const country = "India";
+console.log(country);
+
+
+
+//                 let       Var        Const
+// Redeclare      No          Yes         No
+// Reassign       Yes         Yes         No
